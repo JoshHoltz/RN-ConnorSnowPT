e588100291935f559d6f7d372d807ee69f72ed80
Josh <joshua.holtz@outlook.com>
2025-08-22 15:35:08 +0100

    basic react native template --using expo

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.flex}>Open up App.js to start on your app!</Text>
+      <Text style={styles.flex}>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flex: {
+    color: "green"
+  }
+});
